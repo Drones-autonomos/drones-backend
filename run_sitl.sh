6f256629a5548a8b0057575b26b795736ef9c97d
Fernando Ramírez <dferramm@gmail.com>
2026-09-22 11:37:34 -0600

    feat: add script to start SITL at UAQ Juriquilla

---
+++ run_sitl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Activa el entorno virtual
+source venv/bin/activate
+
+# Coordenadas de la Facultad de Informática UAQ (Campus Juriquilla)
+# Latitud, Longitud, Altitud (metros sobre el nivel del mar), Orientación (grados)
+HOME_LOCATION="20.70428,-100.44358,1900,0"
+
+echo "Iniciando simulador en Facultad de Informática UAQ, Juriquilla..."
+dronekit-sitl copter --home=$HOME_LOCATION
